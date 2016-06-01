1cf1eafc622f83b2e12fa713e5aafc1457e2f814
Radulenko <radulenko@gmail.com>
2016-06-01 10:22:03 +0300

    add project

---
+++ tradulenko/testRepoForQALight/testRepoForQALight/src/mainPackage/TestClass.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Test Git");
+		System.out.println("Test Git2");
+		System.out.println("Test Git3");
+		System.out.println("Test Master3");
+		System.out.println("Test NewBranch");		
+		
+	
+		
+	}
+}
